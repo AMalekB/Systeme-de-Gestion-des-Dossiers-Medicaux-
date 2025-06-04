8c3e597d7ae059e159adf893b8704bc070c3c09b
AMalekB <bezri.a.malek@gmail.com>
2025-06-04 14:11:26 -0400

    Protection admin dashboard frontend

--- app/admin/dashboard/page.jsx
+++ app/admin/dashboard/page.jsx
@@ -1,6 +1,29 @@
-export default function dashboardAdmin() {
+"use client";
+
+import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function DashboardAdmin() {
+  const router = useRouter();
+  const [autorisé, setAutorisé] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const role = localStorage.getItem("role");
+
+    if (!token || role !== "ADMIN") {
+      router.push("/login");
+    } else {
+      setAutorisé(true);
+    }
+  }, [router]);
+
+  if (!autorisé) {
+    return null;
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-sky-100 text-gray-800">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-sky-100 text-gray-800">
       <h1 className="text-4xl font-bold">Bienvenue dans le dashboard admin</h1>
     </main>
   );
